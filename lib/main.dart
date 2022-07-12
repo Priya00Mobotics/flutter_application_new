f3e7bb00b929e27c578fd12ef8a98b35c0f7b26c
Priya00Mobotics <appdevel@mobotics.in>
2022-07-12 12:42:29 +0530

    Day-2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_new/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-            child: Container(
-          child: Text("Welcome to Priyanka's World"),
-        )),
-      ),
+      home: HomePage(),
     );
   }
 }
